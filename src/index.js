0bc2aa1cf0014057f6f4e94712cc949a562f04f9
QungHuyy <qunghuyy17@gmail.com>
2025-06-06 13:21:25 +0700

    Upgrade react-scripts to version 5.0.1, add PerfectScrollbar for enhanced scrolling experience, and include its CSS. Update package-lock.json accordingly.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PerfectScrollbar from 'perfect-scrollbar'; // Thêm dòng này
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
 
 ReactDOM.render(
   <React.StrictMode>
